a07f4a3bfd1fd259abd6b4c812a2269a9e7d1a7e
asaryonex <asaryonex@gmail.com>
2026-07-13 03:55:47 +0530

    Add Railway deployment files

--- config.py
+++ config.py
@@ -3,13 +3,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+print("BOT_TOKEN:", os.getenv("BOT_TOKEN"))
+print("CHECK_INTERVAL:", os.getenv("CHECK_INTERVAL"))
+print("GOODRETURNS_URL:", os.getenv("GOODRETURNS_URL"))
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN environment variable is missing.")
 
 CHECK_INTERVAL = int(os.getenv("CHECK_INTERVAL", "300"))
-
 GOODRETURNS_URL = os.getenv(
     "GOODRETURNS_URL",
     "https://www.goodreturns.in/gold-rates/coimbatore.html",
